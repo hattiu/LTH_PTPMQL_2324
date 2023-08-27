3061cfb9a396618ebcfa24e7911eb321e807fae1
hattiu <hattiu210817@gmail.com>
2023-08-27 17:27:34 +0700

    switch_case

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -4,16 +4,19 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-       int a = 10;
-       if(a<0)
-       {
-        System.Console.WriteLine("{0} la so nguyen am", a);
-       }else{
-        if(a % 2 == 0){
-            System.Console.WriteLine("{0} la so nguyen duong chan",a);
-        }else{
-            System.Console.WriteLine("{0} la so nguyen duong le",a);
-        }
-       }
+      int day = 2;
+      switch(day)
+      {
+        case 1: System.Console.WriteLine("Chu nhat"); break;
+        case 2: System.Console.WriteLine("Thu 2");break;
+        case 3: System.Console.WriteLine("Thu 3"); break;
+        case 4: System.Console.WriteLine("Thu 4");break;
+        case 5: System.Console.WriteLine("Thu 5"); break;
+        case 6: System.Console.WriteLine("Thu 6");break;
+        case 7: System.Console.WriteLine("Thu 7"); break;
+        default :
+        System.Console.WriteLine("khong phai la ngay torng tuan");
+        break;
+      }
     }
 }

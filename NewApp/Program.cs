b0c3671e1d7cafb6f73b706af5fb46a7657e3fd3
hattiu <hattiu210817@gmail.com>
2023-08-27 18:16:04 +0700

    NhapTT_ChoDoiTuong_PS2

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,9 +3,20 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Employee mpl = new Employee ();
+        /*Employee mpl = new Employee ();
         mpl.NhapTT();
-        mpl.HienThiTT();
+        mpl.HienThiTT();*/
+        
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        
+        //gán giá trị cho thuộc tính đối tượng ps1
+
+        ps2.Name = "Nguyan Van B";
+        ps2.Address = "ha noi";
+        ps2.Age = 18;
+        //gọi phương thưcs hiển thịthoong tin
+        ps2.HienThi();
 
     }
 }

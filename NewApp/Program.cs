a27015b6adbd840d0830d3a9febfe440d8abd70d
hattiu <hattiu210817@gmail.com>
2023-08-27 17:01:52 +0700

    chuyen_doi_kieu_du_lieu

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-       //Khai báo và gán giá trị cho 2 hằng số kiểu string và int, sau đó in ra màn hình
-        const string hoTen = "Le thi hoa";
-        const int maSV = 1921050251;
-        Console.WriteLine("Sinh vien {0} - Ma sinh vien {1}", hoTen, maSV);
-
+       string str = "123";
+       int ketQua;
+       bool kiemTra = false;
+       kiemTra = int.TryParse(str,out(ketQua));
+       System.Console.WriteLine("Ket qua = "+ ketQua);
     }
 }

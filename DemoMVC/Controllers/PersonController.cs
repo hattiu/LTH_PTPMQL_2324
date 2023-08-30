90fec10a938b754f5c728a623ae58e7cc04e707c
hattiu <hattiu210817@gmail.com>
2023-08-30 18:15:11 +0700

    xd_PersonContro

---
+++ DemoMVC/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(string Name, string DiaChi)
+        {
+            ViewBag.thongBao = "Xin chao: " + Name +"-"+ DiaChi;
+            return View();
+        }
+
+    }
+}
